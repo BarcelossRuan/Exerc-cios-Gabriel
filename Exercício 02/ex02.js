a91112cc2b1b77a74c1f70feac1968b0daf64aa6
BarcelossRuan <ruan.elesis@escola.pr.gov.br>
2026-07-09 09:33:18 -0300

    Add light and dark mode functions

---
+++ Exercício 02/ex02.js
@@ -0,0 +1,16 @@
+function modoClaro() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+
+
+
+document.getElementById("menssagem").innerText = "Modo claro ativado!";
+}
+
+function modoEscuro() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+
+
+document.getElementById("menssagem").innerText = "Modo escuro ativado!";
+}
